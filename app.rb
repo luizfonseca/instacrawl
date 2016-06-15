3e50047a71c0fdd403360801023131bb954e52fa
Luiz Fonseca <eu@luiz.cc>
2016-06-14 21:35:49 -0300

    Now media/search respond to json and outputs the same as instagram

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/json'
+require 'json'
 
 set :database, { adapter: "sqlite3", database: "database.sqlite3" }
 
@@ -11,7 +12,7 @@ Dir.glob('./models/*.rb').each { |r| require r }
 
 
 get '/api/' do
-
+  return ""
 end
 
 
@@ -19,14 +20,32 @@ end
 
 
 get '/api/locations' do
+  content_type :json
+  
 	@locations = Location.all
-	json @locations
+  JSON.pretty_generate(@locations.as_json)
 end
 
 get '/api/media' do
 	@medias = Media.all
 	json @medias
 end
+
+
+get '/api/media/search' do
+  @medias = nil
+  lat = params[:lat]
+  lng = params[:lng]
+  if lat && lng 
+    @medias = Media.find_by_coord(lat,lng) 
+  end
+
+  JSON.pretty_generate(@medias.as_json)
+end
+
+
+
+
 
 get '/api/locations/:location' do
 	
--- models/media.rb
+++ models/media.rb
@@ -4,12 +4,50 @@ class Media < ActiveRecord::Base
 	belongs_to :location
 	validates_presence_of :location
 
-	#def as_json(options={})
-		#return {
-
-		#}.merge(options)
+	def as_json(options={})
+    return {
+      distance: 0.0,
+      type: self.type,
+      users_in_photo: [],
+      filter: "",
+      tags: self.location.hashtag,
+      comments: {
+        count: self.comments_count
+      },
+      caption: self.caption,
+      likes: {
+        count: self.likes_count
+      },
+      user: {
+        id: self.owner_id
+      },
+      link: "https://instagram.com/p/#{self.shortcode}",
+      created_time: self.date,
+      images: {
+        low_resolution: {},
+        thumbnail: {
+          url: self.remote_thumb_src,
+          width: self.dimension_w,
+          height: self.dimension_h
+        },
+        standard_resolution: {
+          url: self.remote_display_src,
+          width: self.dimension_w,
+          height: self.dimension_h
+        }
+      }
+    }.merge(options)
 		
-	#end
+	end
 
+  def type
+    is_video ? 'video' : 'image'
+  end
+
+  def self.find_by_coord(lat, lng)
+    self.find_by_sql ["SELECT * FROM medias as md 
+    JOIN locations as lc ON lc.id = md.location_id 
+    WHERE lc.lat = ? AND lc.lng = ?", lat, lng]
+  end
 end
 
