6952941ef36c4e187b456338d69ddab13187e68f
Luiz Fonseca <eu@luiz.cc>
2016-06-17 13:45:15 -0300

    Fix issue with Time/zone on media/search endpoint

--- models/media.rb
+++ models/media.rb
@@ -1,4 +1,4 @@
-require 'date'
+require 'time'
 
 class Media < ActiveRecord::Base
 	self.table_name = "medias" 
@@ -68,12 +68,12 @@ class Media < ActiveRecord::Base
 
   def self.find_by_coord_and_time(lat, lng, time_options = {})
     
-    date = time_options[:date].nil? ? Time.now.strftime('%Y-%m-%d') : DateTime.parse(time_options[:date]).strftime('%Y-%m-%d')
+    date = time_options[:date].nil? ? Time.now.strftime('%Y-%m-%d') : Time.parse(time_options[:date]).strftime('%Y-%m-%d')
     from = time_options[:from].nil? ? "#{Time.now.hour - 1}:00" : time_options[:from]
     to   = time_options[:to].nil?  ? "#{Time.now.hour}:00" : time_options[:to]
 
-    from_date = DateTime.parse("#{date} #{from}")
-    to_date   = DateTime.parse("#{date} #{to}")
+    from_date = Time.parse("#{date} #{from}")
+    to_date   = Time.parse("#{date} #{to}")
 
     self.find_by_sql ["SELECT * FROM medias as md 
     JOIN locations as lc ON lc.id = md.location_id 
